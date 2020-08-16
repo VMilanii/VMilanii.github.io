778bf4ee3d5ddc2de4608daa0d78465d5e7f3ed7
Victor Milani <vitor-milani@hotmail.com>
2020-08-16 19:42:32 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+	new SweetScroll({});
+  
+  particlesJS.load('particles-js', './assets/config/particles.json', function() {
+    console.log('particles.js loaded - callback');
+  }); //inicializar particles
+
+  
+}, !1);
